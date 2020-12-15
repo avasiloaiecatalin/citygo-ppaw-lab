25f3e7922f4c1c6c08f3127f965ae76315d23b21
ezli <avasiloaiecatalin@gmail.com>
2020-12-15 18:38:02 +0200

    Update init-models.js

--- src/modelss/init-models.js
+++ src/modelss/init-models.js
@@ -1,15 +1,17 @@
 var DataTypes = require("sequelize").DataTypes;
 var _clients = require("./clients");
 var _scooters = require("./scooters");
+var _rents = require("./rents");
 
 function initModels(sequelize) {
   var clients = _clients(sequelize, DataTypes);
   var scooters = _scooters(sequelize, DataTypes);
-
+  var rents = _rents(sequelize, DataTypes);
 
   return {
     clients,
     scooters,
+    rents
   };
 }
 module.exports = initModels;
